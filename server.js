accf85e9e875a9d5711d54a4f889cea167d9bdb0
29004228 <p.b.dupire@gmail.com>
2019-10-31 15:25:43 +0100

    added newrelic

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('newrelic');
+
 //server.js
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+
+
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
 
